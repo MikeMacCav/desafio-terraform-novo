657738f2d4466631fb3feda70e2d9566f674fa63
MikeMacCav <michaelmcavalcante@gmail.com>
2025-04-13 02:55:51 -0300

    Ajustados arquivos para fazer o Deploy pelo Terraform utilizando Ansible.

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Definir IP da instância e caminho da chave privada
-EC2_HOST="ubuntu@44.211.144.79"
+EC2_HOST="ubuntu@44.211.160.19"
 SSH_KEY="F:/terraform/keys/terraform-key-ec2.pem"
 
 echo "=====> Criando diretórios no servidor..."
-ssh -i $SSH_KEY $EC2_HOST "mkdir -p /home/ubuntu/desafio-terraform-novo/apache /home/ubuntu/desafio-terraform-novo/mysql"
+ssh -i $SSH_KEY $EC2_HOST "mkdir -p /home/ubuntu/desafio-terraform-novo/apache/public-html /home/ubuntu/desafio-terraform-novo/apache /home/ubuntu/desafio-terraform-novo/mysql"
 
 echo "=====> Copiando arquivos do Apache..."
 scp -i $SSH_KEY -r ./apache/* $EC2_HOST:/home/ubuntu/desafio-terraform-novo/apache/
@@ -15,13 +15,19 @@ scp -i $SSH_KEY -r ./mysql/* $EC2_HOST:/home/ubuntu/desafio-terraform-novo/mysql
 
 echo "=====> Criando containers Docker na EC2..."
 ssh -i $SSH_KEY $EC2_HOST << 'EOF'
-    # Criar imagem Apache
-    docker build -t apache_php /home/ubuntu/desafio-terraform-novo/apache
-    docker run -d --name apache_container -p 80:80 -v /home/ubuntu/desafio-terraform-novo/html:/var/www/html apache_php
+    # Entrar no diretório Apache e construir a imagem
+    cd /home/ubuntu/desafio-terraform-novo/apache
+    docker build -t apache_php .
 
-    # Criar imagem MySQL
-    docker build -t mysql_custom /home/ubuntu/desafio-terraform-novo/mysql
-    docker run -d --name mysql_container --env-file /home/ubuntu/desafio-terraform-novo/mysql/.env -p 3306:3306 mysql_custom
+    # Rodar o container Apache
+    docker run -d --name apache_container -p 80:80 -v /home/ubuntu/desafio-terraform-novo/apache/public-html:/usr/local/apache2/htdocs/ apache_php
+
+    # Entrar no diretório MySQL e construir a imagem
+    cd /home/ubuntu/desafio-terraform-novo/mysql
+    docker build -t mysql_custom .
+
+    # Rodar o container MySQL
+    docker run -d --name mysql_container --env MYSQL_ROOT_PASSWORD=root --env MYSQL_DATABASE=desafio --env MYSQL_USER=admin --env MYSQL_PASSWORD=metroid -p 3306:3306 mysql_custom
 EOF
 
 echo "=====> Deploy finalizado!"
--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 provider "aws" {
-  region = "us-east-1" 
-  # profile = default 
+  region = "us-east-1"
+   profile = "default"  
 }
 
 resource "aws_security_group" "instance_sg" {
   name        = "instance_security_group"
   description = "Permitir acesso SSH, HTTP e MySQL"
-  vpc_id      = "vpc-03264446b721cc5f1"
+  vpc_id      = "vpc-03264446b721cc5f1"  
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["179.255.125.210/32"]
+    cidr_blocks = ["179.255.125.210/32"]  
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   egress {
@@ -38,16 +38,16 @@ resource "aws_security_group" "instance_sg" {
 }
 
 data "aws_instance" "ec2_instance" {
-  instance_id = "i-02b9092fcaaf643b6" 
+  instance_id = "i-02b9092fcaaf643b6"  
 }
 
-  resource "null_resource" "ansible_deploy" {
-  depends_on = [data.aws_instance.ec2_instance]
-
+resource "null_resource" "ansible_deploy" {
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${data.aws_instance.ec2_instance.public_ip}, --private-key F:/terraform/keys/terraform-key-ec2.pem deploy_apache_mysql.yml"
-    # Se você estiver executando o Terraform no WSL e o Ansible no WSL:
-    # command = "wsl ansible-playbook -i ${data.aws_instance.ec2_instance.public_ip}, --private-key /mnt/f/terraform/keys/terraform-key-ec2.pem deploy_apache_mysql.yml"
-
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i 44.211.160.19, -u ubuntu --private-key /mnt/f/terraform/keys/terraform-key-ec2.pem deploy_apache_mysql.yml --ssh-extra-args='-o StrictHostKeyChecking=no'"
   }
+
+  depends_on = [
+    data.aws_instance.ec2_instance,
+    aws_security_group.instance_sg
+  ]
 }
